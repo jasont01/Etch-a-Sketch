d01e94205575eedc83d48ada423f94cdd54d3d8e
Jason Thompson <jason@jasont.us>
2020-05-28 15:06:06 -0400

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const grid = document.getElementById('grid');
 
+//Resize Button
+document.querySelector('.resize').addEventListener('click', () => {
+  document.querySelectorAll('.cell').forEach((cell) => grid.removeChild(cell));
+  createGrid();
+});
+
+//Reset Button
+const reset = document.querySelector('.reset');
+reset.addEventListener('mouseover', () => document.querySelector('audio').play());
+reset.addEventListener('click', resetGrid);
+
 createGrid();
 
 function createGrid() {
@@ -16,4 +27,8 @@ function createGrid() {
     document.querySelectorAll('.cell').forEach((cell) => {
       cell.addEventListener('mouseenter', () => cell.classList.add('cellON'));
   });
+};
+
+function resetGrid() {
+  document.querySelectorAll('.cell').forEach((cell) => cell.classList.remove('cellON'));
 };
